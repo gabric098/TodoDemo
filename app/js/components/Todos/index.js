9b0b1868f377a7c8fabea8db6e7a066200fdeb60
Gabriele <gabriele.antonini@gmail.com>
2015-04-29 16:43:17 +0200

    Added todo-item directive

--- app/js/components/Todos/index.js
+++ app/js/components/Todos/index.js
@@ -7,5 +7,6 @@ module.exports = angular
         require('angular-ui-router'),
         require('../TodoDB').name
     ])
+    .directive('todoItem', require('./TodoItemDirective'))
     .controller('TodosController', require('./TodosController'))
     .config(require('./states'));
